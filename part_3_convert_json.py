cad7955cd958c1372ce68a0c32741e233dbf91ed
Jasmine <43485610+jasminehliu@users.noreply.github.com>
2019-02-22 13:14:03 -0500

    Part 1 Phase 3

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,5 +1,6 @@
 import cc_dat_utils
 import cc_data
+import json
 
 #Part 3
 #Load your custom JSON file
@@ -10,36 +11,38 @@ import cc_data
 
 
 # Convert Data
-def make_game_library_from_json( json_data ):
+def make_level_library_from_json( json_data ):
     
     level_library = cc_data.CCDataFile()
     
-    for level_data in json_data["games"]:
-        game = test_data.Game()
-        game.title = game_data["title"]
-        game.year = game_data["Year"]
+    for level_data in json_data["levels"]:
+        level = cc_data.CCLevel()
         
-        platform_data = game_data["platform"]
-        platform = test_data.Platform()
-        platform.name = platform_data["name"]
-        platform.launch_year = platform_data["year"]
+        level.level_number = level_data["level"]
+        level.time = level_data["time"]
+        level.num_chips = level_data["chipNum"]
+        level.upper_layer = level_data["Upper_layer"]
         
-        game.platform = platform
         
-        game_library.add_game(game)
-
+        optional_fields = level_data["optional"]
+        hint = cc_data.CCMapHintField(optional_fields["hint"])
+        level.add_field(hint)
+        title = cc_data.CCMapTitleField(optional_fields["title"])
+        level.add_field(title)
+        
+        level_library.add_level(level)
     
-    ### End Add Code Here ###
-
-    return game_library
+    return level_library
 
 # Load Custom Data
 input_json_file = "data/jhliu_cc1.json"
 
 with open(input_json_file, "r") as reader:
     json_data = json.load(reader)
-game_data = make_game_library_from_json(json_data)
+level_data = make_level_library_from_json(json_data)
 
+# Save Data to DAT
+cc_dat_utils.write_cc_data_to_dat(level_data, "jhliu_cc1.dat")
 
 
 
